1eb3a340458f9b3f75fd98840ce8a41f46b2f983
mlzoo <lmloveu@qq.com>
2022-01-07 16:03:27 +0800

    init

---
+++ classification/VGGNet/predict.py
@@ -0,0 +1,40 @@
+from tqdm.notebook import tqdm
+
+import numpy as np
+from PIL import Image
+
+import torch
+from torch import nn, optim
+import torchvision
+from torchvision import transforms
+
+from vggnet import *
+
+def load_checkpoint(model, checkpoint_PATH, optimizer, device='cuda'):
+    print('Loading model...')
+    model_CKPT = torch.load(checkpoint_PATH) # 加载模型
+    
+    model.load_state_dict(model_CKPT['state_dict']) # 替换权重
+    model = model.to(device)
+    
+    print('Loading optimizer...')
+    optimizer.load_state_dict(model_CKPT['optimizer']) # 替换optimizer中的权重
+    
+    print('Loading succeed')
+    return model, optimizer
+
+model = get_VGG16(num_classes=10)# .to(device)
+optimizer = optim.Adam(params = model.parameters(),lr=0.001)
+
+model, optimizer = load_checkpoint(model, './vgg16--epoch-1--accuracy-0.1949.pth.tar', optimizer)
+
+data = Image.open('./car.jpg')
+data = np.array(data) / 255 # [0,1]
+data = data.transpose(2, 0, 1) # 224,224,3 -> 3,224,224
+data = data[np.newaxis, :]
+data = torch.Tensor(data).to('cuda')
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+print('Predicted label :', classes[model(data).argmax()])
